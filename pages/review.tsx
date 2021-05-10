7074a3f34c7dc8eec409d18e8fb7deda860f6ea5
Riley Miller <rmllr000@gmail.com>
2021-05-10 02:09:35 -0600

    feat: can preview coaches in scrape review (#4)
    * feat: can preview coaches in scrape review
    
    * feat: sort by team

--- pages/review.tsx
+++ pages/review.tsx
@@ -7,7 +7,7 @@ import {
 } from 'next-firebase-auth'
 import * as recruitApi from '../recruit-api'
 import Header from '../components/Header'
-
+import Image from 'next/image'
 const styles = {
   content: {
     padding: 32,
@@ -17,13 +17,46 @@ const styles = {
   },
 }
 
-type DemoType = {
-  coaches: Record<string, unknown>[]
+type Coach = {
+  id: string
+  coachName: string
+  needsReview: boolean
+  prodRecordExists: boolean
+  profilePictureURL?: string
+  lastCheckedTime: Date
+  school: string
+  sport: string
+  [key: string]: unknown
+}
+type ReviewType = {
+  coaches: Coach[]
 }
 
-const Demo = ({ coaches }: DemoType) => {
+const ReviewView = ({ coaches }: ReviewType) => {
   const AuthUser = useAuthUser()
 
+  const getPropList = (properties: [string, string | boolean][]) => {
+    let liS = []
+    let i = 0
+    for (const [key, val] of properties) {
+      i++
+      liS.push(
+        <li key={i}
+          style={{
+            listStyleType: 'none',
+            padding: '0.25rem .5rem'
+          }}
+        >
+          <span style={{
+            fontWeight: 'bold'
+          }}>
+            {key}
+          </span>: {val.toString()}
+        </li>
+      )
+    }
+    return liS
+  }
   return (
     <div>
       <Header email={AuthUser.email} signOut={AuthUser.signOut} />
@@ -34,8 +67,89 @@ const Demo = ({ coaches }: DemoType) => {
             This page requires authentication. It will do a server-side redirect
             (307) to the login page if the auth cookies are not set.
           </p>
-          <p>{JSON.stringify(coaches)}</p>
         </div>
+        <section style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}>
+          {coaches.sort((a, b) => {
+            if (a.school < b.school) {
+              return -1
+            }
+
+            if (a.school > b.school) {
+              return 1
+            }
+
+            if (a.school === b.school) {
+
+              if (a.sport < b.sport) {
+                return -1
+              }
+
+              if (a.sport > b.sport) {
+                return 1
+              }
+
+              if (a.sport === b.sport) {
+                if (a.coachName < b.coachName) {
+                  return -1
+                }
+
+                if (a.coachName > b.coachName) {
+                  return 1
+                }
+              }
+
+            }
+            return 0
+          }).map(coach => {
+            const { coachName, profilePictureURL, ...metadata } = coach
+
+            const met = Object.entries(metadata)
+
+            console.log(`metadata: ${metadata}`)
+            return (
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                padding: '2rem',
+                boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+                transition: '0.3s',
+                borderRadius: '.3rem',
+                width: '50vw'
+              }} key={coach.id}>
+                <h2>{coach.coachName}</h2>
+                {profilePictureURL ?
+                  (
+
+                    <div
+                      style={{
+                        height: '250px',
+                        width: '200px',
+                        position: 'relative'
+                      }}
+                    >
+                      <Image src={profilePictureURL} layout="fill" objectFit="contain" quality={100} />
+                    </div>
+                  ) : (<div>No Image</div>)
+                }
+                <ul
+                  style={{
+                    paddingLeft: '0',
+                    alignSelf: 'flex-start'
+                  }}
+                >
+                  {
+                    getPropList(met as [string, string | boolean][])
+                  }
+                </ul>
+              </div>
+            )
+          })}
+        </section>
       </div>
     </div>
   )
@@ -61,6 +175,6 @@ export const getServerSideProps = withAuthUserTokenSSR({
   }
 })
 
-export default withAuthUser<DemoType>({
+export default withAuthUser<ReviewType>({
   whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
-})(Demo)
+})(ReviewView)
