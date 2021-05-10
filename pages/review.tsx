37786e2e05839b542a989890e2882902579988b1
Riley Miller <rmllr000@gmail.com>
2021-05-10 00:26:56 -0600

    refac: remove extra whitespace

--- pages/review.tsx
+++ pages/review.tsx
@@ -44,8 +44,6 @@ const Demo = ({ coaches }: DemoType) => {
 export const getServerSideProps = withAuthUserTokenSSR({
   whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
 })(async ({ AuthUser, req }) => {
-
-
   const res = await fetch(recruitApi.REVIEW_URL)
   const recruitData = await res.json()
 
