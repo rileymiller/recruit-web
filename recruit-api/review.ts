b3fc13da8c44b790d3e0c40d0f835c730de20186
Riley Miller <rmllr000@gmail.com>
2021-05-11 18:23:15 -0600

    refac: make the env vars public

--- recruit-api/review.ts
+++ recruit-api/review.ts
@@ -2,8 +2,9 @@ import { Coach } from "../pages/review"
 
 export const REVIEW_URL = `${process.env.RECRUIT_API_BASE_URL}${process.env.RECRUIT_API_REVIEW_ENDPOINT}`
 
-export const GET_COACH_URL = `${process.env.RECRUIT_API_BASE_URL}${process.env.RECRUIT_API_COACH_ENDPOINT}`
+export const GET_COACH_URL = `${process.env.NEXT_PUBLIC_RECRUIT_API_PROD_URL}${process.env.NEXT_PUBLIC_RECRUIT_API_COACH_ENDPOINT}`
 export const getCoach = async (coachID: string) => {
+  console.log(`GET_COACH_URL: ${GET_COACH_URL}`)
   const rawData = await fetch(`${GET_COACH_URL}/${coachID}`)
 
   const coach: Coach = await rawData.json()
